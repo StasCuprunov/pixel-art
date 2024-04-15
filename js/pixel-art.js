37a6fea79168e6f2501b0e6b72b1954e9d4b2fa9
Stas Cuprunov <stas.cuprunov@t-online.de>
2024-04-15 14:05:12 +0200

    implement generateGrid and grid changes if you change the grid size

--- js/pixel-art.js
+++ js/pixel-art.js
@@ -1,12 +1,42 @@
 function generateGridSizeOptions() {
-    var listOfGridSizes = [8, 12, 16, 32];
+    let listOfGridSizes = [8, 12, 16, 32];
 
-    var gridSizeContainer = document.getElementById("gridSize");
+    let gridSizeContainer = document.getElementById("gridSize");
 
     listOfGridSizes.forEach((size) => {
-        var option = document.createElement("option");
+        let option = document.createElement("option");
         option.setAttribute("value", size);
         option.innerHTML = size + " x " + size;
         gridSizeContainer.appendChild(option);
     });
+}
+
+function generateGrid() {
+    let size = document.getElementById("gridSize").value;
+    let lastColumn = size - 1;
+    let indexBorder = size * size;
+
+    let gridContainer = document.getElementById("grid");
+
+    for (let index = 0; index < indexBorder; index++) {
+        let input = document.createElement("input");
+        input.setAttribute("type", "button");
+        input.setAttribute("id", "pixel-" + index);
+        input.setAttribute("name", "grid");
+        gridContainer.appendChild(input);
+
+        let isLastColumn = (index % size) === lastColumn;
+        if (isLastColumn) {
+            let lineBreak = document.createElement("br");
+            gridContainer.appendChild(lineBreak);
+        }
+    }
+}
+
+function changeGrid() {
+    // remove grid
+    let gridContainer = document.getElementById("grid");
+    gridContainer.textContent = '';
+
+    generateGrid();
 }
